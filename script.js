6a15da44729bacac140db02b7c3a7cb43f250528
Temaigroman <152429841+Temaigroman@users.noreply.github.com>
2024-10-19 16:33:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const slider = document.getElementById("slider");
+const arrowLeft = document.querySelector(".arrow-left");
+const arrowRight = document.querySelector(".arrow-right");
+const slides = document.querySelectorAll(".slider-image");
+const bottom = document.getElementById("bottom");
+
+let currentSlideIndex = 0;
+const paginationCircles = [];
+const sliderWidth = slider.clientWidth;
+const images = [
+    '../assets/gallery1_min.svg',
+    '../assets/gallery2_min.svg',
+    '../assets/gallery1_min.svg',
+    '../assets/gallery1_min.svg',
+];
+
+function createPaginationCircle() {
+    const div = document.createElement("div");
+    div.className = "pagination-circle";
+    bottom.appendChild(div);
+    paginationCircles.push(div);
+
+    for (let i = 0; i < images.length; i++) {
+        const img = new Image();
+        img.src = images[i];
+        img.style.display = 'none';
+        div.appendChild(img);
+        div.style.backgroundImage = `url(${images[i]})`;
+    }
+}
+
+
+function addPagination() {
+    slides.forEach(createPaginationCircle);
+    paginationCircles[0].classList.add("active");
+    paginationCircles.forEach((circle, index) => {
+        circle.addEventListener("click", () => changeSlide(index));
+    });
+}
+
+function addActiveClass() {
+    paginationCircles[currentSlideIndex].classList.add("active");
+}
+
+function removeActiveClass() {
+    paginationCircles[currentSlideIndex].classList.remove("active");
+}
+
+function showSlide() {
+    slider.style.transform = `translateX(-${currentSlideIndex * sliderWidth}px)`;
+}
+
+function changeSlide(slideIndex) {
+    removeActiveClass();
+    currentSlideIndex = slideIndex;
+    addActiveClass();
+    showSlide();
+}
+
+function nextSlide() {
+    let newSlideIndex = currentSlideIndex + 1;
+    if(newSlideIndex > slides.length - 1) {
+        newSlideIndex = 0;
+    }
+    changeSlide(newSlideIndex);
+}
+
+function previousSlide() {
+    let newSlideIndex = currentSlideIndex - 1;
+    if(newSlideIndex < 0) {
+        newSlideIndex = slides.length - 1;
+    }
+    changeSlide(newSlideIndex);
+}
+
+addPagination();
+arrowLeft.addEventListener("click", previousSlide);
+arrowRight.addEventListener("click", nextSlide);
